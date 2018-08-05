70fd0795b7a7580f6f9d40b9b4e95d0e809a909e
puppe1990 <matheus.puppe90@hotmail.com>
2018-08-05 17:15:14 -0300

    Melhorias Destaque

--- app/Http/Controllers/DestaqueController.php
+++ app/Http/Controllers/DestaqueController.php
@@ -13,20 +13,34 @@ class DestaqueController extends Controller
     public function listar(){
 
         $destaques = Produto
-        ::leftJoin('entradas', 'produtos.id_produto', '=', 'entradas.fk_produto')
-        ->leftJoin(DB::raw('(select produtos.id_produto,sum(saidas.quantidade) as quantidadeSaida
-                            from saidas 
-                            inner join produtos on produtos.id_produto = saidas.fk_produto
-                            group by produtos.id_produto) as temp'), 'temp.id_produto', '=', 'produtos.id_produto')
-        ->join('categorias', 'categorias.id_categoria', '=', 'produtos.fk_categoria')
-        ->select('produtos.id_produto','produtos.codigo_produto','produtos.descricao', 'produtos.valor',DB::raw('sum(entradas.quantidade) as quantidadeEntrada'),'temp.quantidadeSaida','categorias.nome','produtos.path_image as imagens')
-        ->where('destaque', '=', 1)
-        ->groupBy('produtos.descricao','produtos.codigo_produto','produtos.valor','produtos.id_produto','categorias.nome','produtos.path_image','temp.quantidadeSaida')
-        ->getQuery() // Optional: downgrade to non-eloquent builder so we don't build invalid User objects.
-        ->orderBy('produtos.id_produto','ASC')
-        ->get();
+            ::leftJoin('entradas', 'produtos.id_produto', '=', 'entradas.fk_produto')
+            ->leftJoin(DB::raw('(select produtos.id_produto,sum(saidas.quantidade) as quantidadeSaida
+                                from saidas 
+                                inner join produtos on produtos.id_produto = saidas.fk_produto
+                                group by produtos.id_produto) as temp'), 'temp.id_produto', '=', 'produtos.id_produto')
+            ->join('categorias', 'categorias.id_categoria', '=', 'produtos.fk_categoria')
+            ->select('produtos.id_produto','produtos.codigo_produto','produtos.descricao', 'produtos.valor',DB::raw('sum(entradas.quantidade) as quantidadeEntrada'),'temp.quantidadeSaida','categorias.nome','produtos.path_image as imagens')
+            ->where('destaque', '=', 1)
+            ->groupBy('produtos.descricao','produtos.codigo_produto','produtos.valor','produtos.id_produto','categorias.nome','produtos.path_image','temp.quantidadeSaida')
+            ->getQuery() // Optional: downgrade to non-eloquent builder so we don't build invalid User objects.
+            ->orderBy('produtos.id_produto','ASC')
+            ->get();
 
-    	return view('destaque.listagem')->with(['destaques' => $destaques]);
+        $linhaTempoEntrada = Produto
+            ::join('entradas', 'produtos.id_produto', '=', 'entradas.fk_produto')
+            ->select(DB::raw("'Entrada' as transacao"),'entradas.created_at as data','entradas.quantidade','produtos.id_produto')
+            ->where('destaque', '=', 1);        
+
+        $linhaTempoGeral = Produto
+            ::join('saidas', 'produtos.id_produto', '=', 'saidas.fk_produto')
+            ->select(DB::raw("'Saida' as transacao"),'saidas.created_at as data','saidas.quantidade','produtos.id_produto')
+            ->where('destaque', '=', 1)
+            ->getQuery() // Optional: downgrade to non-eloquent builder so we don't build invalid User objects.
+            ->union($linhaTempoEntrada)
+            ->orderBy('data','ASC')
+            ->get();
+
+    	return view('destaque.listagem')->with(['destaques' => $destaques,'linhaTempoGeral' => $linhaTempoGeral]);
     }
 
 }
--- resources/views/destaque/listagem.blade.php
+++ resources/views/destaque/listagem.blade.php
@@ -9,21 +9,46 @@
     </div>
   @endif
 
-  
   <div class="row">
   @foreach($destaques as $destaque)  
     <div class="col-sm-6 col-md-4">
       <div class="thumbnail">
-        <img src="{{ $destaque->imagens }}" width="300">
+        <img src="{{ $destaque->imagens }}" width="30%">
         <div class="caption">
           <h3>{{ $destaque->descricao }}</h3>
           <p>Entradas: {{ $destaque->quantidadeEntrada ? $destaque->quantidadeEntrada : 0  }}</p>
           <p>Saídas: {{ $destaque->quantidadeSaida ? $destaque->quantidadeSaida : 0  }}</p>
           <p>Total: {{ $destaque->quantidadeEntrada - $destaque->quantidadeSaida }}</p>
         </div>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Transação</th>
+              <th>Data</th>
+              <th>Quantidade</th>
+              <th>Dias</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($linhaTempoGeral as $linha)
+              <tr>  
+                @if($destaque->id_produto == $linha->id_produto)
+                  <td> {{ $linha->transacao }}</td>
+                  <td>{{ date('d/m/Y', strtotime($linha->data)) }}</td>
+                  <td>{{ $linha->quantidade }}</td>
+                  <?php $diferenca = strtotime('today') - strtotime($linha->data) ?>
+                  <td>{{ $dias = floor($diferenca / (60 * 60 * 24)) }}</td>
+                @endif
+              </tr>                   
+            @endforeach
+          </tbody>  
+        </table>
       </div>
     </div>
   @endforeach
+
+
+
 </div>
 
   
