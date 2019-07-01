05d82f46c9bdfae19694e3ede58c380513b217c5
puppe1990 <matheus.puppe@zaitt.com.br>
2019-06-30 23:27:42 -0300

    List grupo produtos correctly

--- app/Http/Controllers/GrupoProdutoController.php
+++ app/Http/Controllers/GrupoProdutoController.php
@@ -6,14 +6,16 @@ use Request;
 use App\GrupoProduto;
 use App\Produto;
 use App\Http\Requests\GrupoProdutosRequest;
-
+use DB;
 
 class GrupoProdutoController extends Controller
 {
     public function listar(){
-        $grupoProdutos = GrupoProduto::all();
+        $grupoProdutos = GrupoProdutoController::queryContagemGrupoProdutos();
+        $produtos = GrupoProdutoController::queryAllProducts();
         return view('grupoproduto.listagem')
-               ->with(['grupoProdutos' => $grupoProdutos]);
+               ->with(['grupoProdutos' => $grupoProdutos, 
+                      'produtos' => $produtos]);
     }
 
     public function novo(){
@@ -68,4 +70,48 @@ class GrupoProdutoController extends Controller
         return redirect()
                ->action('GrupoProdutoController@listar');
     }
+
+    private function queryContagemGrupoProdutos(){
+        return Produto
+            ::leftJoin('entradas', 'produtos.id_produto', '=', 'entradas.fk_produto')
+            ->leftJoin(DB::raw('(select produtos.id_produto,sum(saidas.quantidade) as quantidadeSaida
+                                from saidas 
+                                inner join produtos on produtos.id_produto = saidas.fk_produto
+                                group by produtos.id_produto) as temp'), 'temp.id_produto', '=', 'produtos.id_produto')
+            ->join('grupo_produtos', 'grupo_produtos.produto_id', '=', 'produtos.id_produto')
+            ->select('grupo_produtos.nome',
+                     DB::raw('sum(entradas.quantidade) as quantidadeEntrada'),
+                     DB::raw('sum(temp.quantidadeSaida) as quantidadeSaida'))
+            ->groupBy('grupo_produtos.nome')
+            ->getQuery() // Optional: downgrade to non-eloquent builder so we don't build invalid User objects.
+            ->get();
+    }
+
+    private function queryAllProducts(){
+        return Produto
+               ::leftJoin('entradas', 'produtos.id_produto', '=', 'entradas.fk_produto')
+               ->leftJoin(DB::raw('(select produtos.id_produto,sum(saidas.quantidade) as quantidadeSaida
+                                    from saidas
+                                    inner join produtos
+                                    on produtos.id_produto = saidas.fk_produto
+                                    group by produtos.id_produto) as temp'), 'temp.id_produto', '=', 'produtos.id_produto')
+               ->join('grupo_produtos', 'grupo_produtos.produto_id', '=', 'produtos.id_produto')
+               ->select('produtos.id_produto',
+                        'produtos.codigo_produto',
+                        'produtos.descricao',
+                        'produtos.valor',
+                        'grupo_produtos.nome',
+                        DB::raw('sum(entradas.quantidade) as quantidadeEntrada'),
+                        'temp.quantidadeSaida')
+               ->groupBy('produtos.descricao',
+                         'produtos.codigo_produto',
+                         'produtos.valor',
+                         'produtos.id_produto',
+                         'grupo_produtos.nome',
+                         'temp.quantidadeSaida')
+               ->getQuery() // Optional: downgrade to non-eloquent builder so we don't build invalid User objects.
+               ->orderBy('produtos.id_produto','ASC')
+               ->get();
+    }
+    
 }
--- resources/views/grupoproduto/listagem.blade.php
+++ resources/views/grupoproduto/listagem.blade.php
@@ -13,47 +13,37 @@
     @endif
 
     <div class="row">
-    @foreach($grupoProdutos as $grupoProduto)  
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-        <img src="{{ $grupoProduto->imagens }}" width="30%">
-        <div class="caption">
-            <h3>{{ $grupoProduto->descricao }}</h3>
-            <p>Entradas: {{ $grupoProduto->quantidadeEntrada ? $grupoProduto->quantidadeEntrada : 0  }}</p>
-            <p>Saídas: {{ $grupoProduto->quantidadeSaida ? $grupoProduto->quantidadeSaida : 0  }}</p>
-            <p>Total: {{ $grupoProduto->quantidadeEntrada - $grupoProduto->quantidadeSaida }}</p>
+    @foreach($grupoProdutos as $grupoProduto)
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <h3>Nome do Grupo: {{ $grupoProduto->nome }}</h3>
+                <p>Entrada: {{ $grupoProduto->quantidadeEntrada }}</p>
+                <p>Saída: {{ $grupoProduto->quantidadeSaida }}</p>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nome</th>
+                            <th>Entrada</th>
+                            <th>Saída</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($produtos as $produto)
+                            <tr>
+                                @if($grupoProduto->nome == $produto->nome)
+                                    <td>{{ $produto->id_produto }}</td>
+                                    <td>{{ $produto->descricao }}</td>
+                                    <td>{{ $produto->quantidadeEntrada }}</td>
+                                    <td>{{ $produto->quantidadeSaida }}</td>
+                                @endif
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th>Transação</th>
-                <th>Data</th>
-                <th>Quantidade</th>
-                <th>Dias</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($linhaTempoGeral as $linha)
-                <tr>  
-                @if($grupoProduto->id_produto == $linha->id_produto)
-                    <td> {{ $linha->transacao }}</td>
-                    <td>{{ date('d/m/Y', strtotime($linha->data)) }}</td>
-                    <td>{{ $linha->quantidade }}</td>
-                    <?php $diferenca = strtotime('today') - strtotime($linha->data) ?>
-                    <td>{{ $dias = floor($diferenca / (60 * 60 * 24) + 1) }}</td>
-                @endif
-                </tr>                   
-            @endforeach
-            </tbody>  
-        </table>
-        </div>
-    </div>
     @endforeach
-
-
-
 </div>
-
-  
 </div>
 @stop
