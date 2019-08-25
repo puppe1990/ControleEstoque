23698926c4214792c5aede9d623d29faf3599da5
puppe1990 <matheus.puppe90@hotmail.com>
2019-08-25 18:36:34 -0300

    lib cors

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
 
     ],
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -77,13 +77,6 @@ $(document).ready(function(){
 
     $("#categoria").select2(); 
 	$("#nomeClientes").select2(); 
-	
-	//Tive que usar este trecho pois não consegui converter o html_entities que estava retornando
-    // $("td:nth-child(2)").each(function(){
-	//     var $this = $(this);
-	//     var t = $this.text();
-	//     $this.html(t.replace('&lt','<').replace('&gt', '>'));
-	// }); 
 
 });
 
